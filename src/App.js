aca3ce7cc994a5cb598166cbadab292409bef40e
Nayan Thara M <wittyhuminid@protonmail.com>
2022-07-01 00:07:10 +0530

    Setup Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react'
 import Gallery from './component/Gallery';
-// import Navbar from './component/Navbar';
-
+import Navbar from './component/Navbar';
+import { Switch, Route, Redirect } from 'react-router-dom';
+// import { Navbar } from 'react-bootstrap';
 function App() {
   return (
-    <div className="container">
-      <Gallery />
-    </div>
+    <>
+      <Navbar />
+      {/* <Gallery /> */}
+    </>
   );
 }
 
